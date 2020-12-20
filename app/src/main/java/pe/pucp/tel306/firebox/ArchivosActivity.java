78ee09f2730f252c17d1f82909cc2d269d664019
Julio Carrion <julio@itic.com.pe>
2020-12-20 11:38:53 -0500

    Subida de archivos

--- app/src/main/java/pe/pucp/tel306/firebox/ArchivosActivity.java
+++ app/src/main/java/pe/pucp/tel306/firebox/ArchivosActivity.java
@@ -1,6 +1,7 @@
 package pe.pucp.tel306.firebox;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -8,11 +9,22 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageMetadata;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class ArchivosActivity extends AppCompatActivity {
 
@@ -28,23 +40,97 @@ public class ArchivosActivity extends AppCompatActivity {
 
 
 
-    public void subirArchivo(View view) {
+    public void pickFile(View view){
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("*/*");
+        startActivityForResult(Intent.createChooser(intent,"Seleccione Archivo para subir"),10);
 
-        int permiso = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
-        if (permiso == PackageManager.PERMISSION_GRANTED) {
-            StorageReference storageReference = FirebaseStorage.getInstance().getReference();
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 10:
+                if (resultCode == RESULT_OK) {
+                    Uri filePath = data.getData();
+                    System.out.println(filePath);
+                    subirArchivoConPutFile(filePath);
+                }
+                break;
         }
     }
 
+    ///METODOS STORAGE - ROYER
 
+    public void subirArchivoConPutFile(Uri uri) {
+        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+            StorageMetadata storageMetadata = new StorageMetadata.Builder()
+                    .setCustomMetadata("autor", "Grupo 5")
+                    .build();
+
+            UploadTask task = storageReference
+                    .child(firebaseUser.getUid()).child("Prueba.jpg") ///nombre a colocar en firebase
+                    .putFile(uri, storageMetadata);
+
+
+            task.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Log.d("infoApp", "subida exitosa");
+                }
+            });
+            task.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.d("infoApp", "error en la subida");
+                    e.printStackTrace();
+                }
+            });
+            task.addOnCanceledListener(new OnCanceledListener() {
+                @Override
+                public void onCanceled() {
+
+                }
+            });
+            task.addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+
+                    long bytesTransferred = snapshot.getBytesTransferred();
+                    long totalByteCount = snapshot.getTotalByteCount();
+
+                    double progreso = (100.0 * bytesTransferred) / totalByteCount;
+
+                    Log.d("progreso", String.valueOf(progreso));
+
+                }
+            });
+
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 2);
+        }
+    }
+
+    //en caso el permiso sea exitoso o denegado:
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (grantResults.length > 0 &&
-                grantResults[0] == PackageManager.PERMISSION_GRANTED &&
-                requestCode == 1) {
-
+                grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (requestCode == 1) {
+                //subirArchivoPutStream(null);
+            } else if (requestCode == 2) {
+                //subirArchivoConPutFile(null);
+            } else if (requestCode == 3) {
+                //descargarDocumento(null);
+            }
         }
     }
 }
